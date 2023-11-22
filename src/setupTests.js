ec8f93564ad4acd7cd07157b1a3397b27a63a5fb
Emanuele Colabello <emanuele.colabello@gmail.com>
2023-11-22 12:32:00 +0100

    Migrate from CRA to Vite, upgrade React to v18.

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,12 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import matchers from '@testing-library/jest-dom/matchers';
+import { expect, vi } from 'vitest';
+
+// This is currently required to use waitFor (react) together with
+// useFakeTimer (vitest), since fake timers support in waitFor relies
+// on jest global symbol.
+// See https://github.com/vitest-dev/vitest/issues/3117#issuecomment-1493249764
+globalThis.jest = vi;
+expect.extend(matchers);
