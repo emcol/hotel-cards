ebcd8b297a1bf4bf2ae8b40e4690e8b5e19337fb
Emanuele Colabello <emanuele.colabello@gmail.com>
2023-11-24 15:13:22 +0100

    vite.config.js: improve code format.

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react({
-    jsxImportSource: '@emotion/react'
-  })]
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+    })
+  ]
 });
